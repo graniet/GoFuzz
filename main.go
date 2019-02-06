93df41c64b475d3c1d8b3b6c9afd9f4bf5545b38
Tristan Granier <graniet75@gmail.com>
2019-02-06 22:49:43 +0100

    update: custom payload

--- main.go
+++ main.go
@@ -11,18 +11,24 @@ import (
 func main(){
 	parser := argparse.NewParser("Gofuzz", "API Rest fuzzer written in Golang")
 	file := parser.String("f", "file", &argparse.Options{Required:true, Help:"File to API requests dump."})
-	fuzzType := parser.String("t", "type", &argparse.Options{Required: true, Help: "Type of fuzzing: SQL, XSS"})
+	fuzzType := parser.String("t", "type", &argparse.Options{Required: false, Help: "Type of fuzzing: SQL, XSS"})
 	printPayload := parser.Flag("v", "verbose", &argparse.Options{Required:false, Help:"Print payload verbose in checking process"})
 	customPayload := parser.String("p", "payload", &argparse.Options{Required:false, Help:"Custom payload file"})
 
 	err := parser.Parse(os.Args)
 	if err != nil{
-		fmt.Print(parser.Usage(err))
+		fmt.Print(parser.Usage("error: " + err.Error()))
 		return
 	}
 	fmt.Printf("\n")
 	figure.NewFigure("GoFuzz", "colossal", true).Print()
 	fmt.Printf("\n")
+
+	if *customPayload == "" && *fuzzType == ""{
+
+		fmt.Print("error: Please set one type or custom payload path.")
+		return
+	}
 
 	Flag := fuzzer.Flag{
 		Verbose: *printPayload,
